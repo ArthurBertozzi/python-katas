68ccbbbfadf869659c780555f0f68a0417535c4f
Arthur Bertozzi <arthur_ba@hotmail.com>
2023-11-09 21:56:17 -0300

    minor adjustment

--- codewars/naughty-or-nice/main.py
+++ codewars/naughty-or-nice/main.py
@@ -1,5 +1,7 @@
 """
-In this kata, you will write a function that receives an array of string and returns a string that is either 'naughty' or 'nice'. Strings that start with the letters b, f, or k are naughty. Strings that start with the letters g, s, or n are nice. Other strings are neither naughty nor nice.
+In this kata, you will write a function that receives an array of string and returns a 
+string that is either 'naughty' or 'nice'. Strings that start with the letters b, f, or k are naughty. 
+Strings that start with the letters g, s, or n are nice. Other strings are neither naughty nor nice.
 
 If there is an equal amount of bad and good actions, return 'naughty'
 
